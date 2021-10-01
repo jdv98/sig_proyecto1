01c7a4d126f02c7e9ebd604d6d80a0503526e214
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2021-09-30 20:22:04 -0600

    closes #7
    correccion

--- sql_scripts/Filtro1.sql
+++ sql_scripts/Filtro1.sql
@@ -1,18 +1,18 @@
+--Recibe los identificadores de las terrazas
 CREATE OR REPLACE FUNCTION filtro1(g_id INT[])
-RETURNS TABLE(
-	area_total_terrazas FLOAT
-)
+RETURNS FLOAT
 AS $$
+DECLARE
+	v_geom geometry[];
 BEGIN
-	SELECT area_terrazas,
-       ST_Union(terrazas.g_id) as singlegeom
-	FROM UNNEST(g_id) terrazas
-	GROUP BY area_terrazas;
+	--Transforma el 3857 a 4326 (pero da igual si es el 3857 o el 5367) por problemas de compatibilidad
+	--Ademas genera un arreglo de geometrias para posteriormente realizar la union
+	v_geom=(SELECT ARRAY_AGG( ST_Transform(t.geom3857,4326) ) FROM terrazas as t
+	JOIN UNNEST(g_id) as n ON t.id=n);
 	
-	RETURN QUERY
-	SELECT st_astext(area_terrazas), st_area(area_terrazas) from UNNEST(area_terrazas) area_total_terrazas
-	
-	
+	--Retorna las hectareas
+	RETURN (SELECT (resul/10000) from ST_Area(ST_Union(v_geom),true) as resul);
 END $$
 LANGUAGE plpgsql;
 
+--SELECT * FROM filtro1(ARRAY[1,2]);
