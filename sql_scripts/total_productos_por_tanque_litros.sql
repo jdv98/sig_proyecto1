ad5fab1acea6195aa47c63ba3f6af37e4c013237
Jose Daniel Villegas Paniagua <josedanielvillegaspaniagua@outlook.com>
2021-10-02 05:54:21 -0600

    segmento eliminado

--- sql_scripts/total_productos_por_tanque_litros.sql
+++ sql_scripts/total_productos_por_tanque_litros.sql
@@ -1,6 +1,5 @@
 --Dosis del producto (en litros)/Total de Llenadas de tanque por hectárea (números de vuelos por hectárea)
 --Devuelte un arreglo con los litros
-DROP FUNCTION total_producto_por_tanque_litros(dosis FLOAT[],total_llenadas_tanque_hectarea FLOAT);
 CREATE OR REPLACE FUNCTION total_producto_por_tanque_litros(dosis FLOAT[],total_llenadas_tanque_hectarea FLOAT)
 RETURNS FLOAT[]
 AS $$
